7f3de52522aa21a9012e1cd8461bbc2f36e485f9
dhaval210 <113909373+dhaval210@users.noreply.github.com>
2023-01-10 15:00:23 +0530

    RUN-1505

--- rungisapps/account_global_discount/__manifest__.py
+++ rungisapps/account_global_discount/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Global ',
-    'version': '12.0.1.2.2',
+    'version': '12.0.1.24',
     'category': 'Accounting',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
